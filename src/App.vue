2734f1b38083b4ab961f4e0e5a8e27a63def0748
cxyxiaoyuyu.com <923996381@qq.com>
2020-10-03 12:38:15 +0800

    完成游戏基本功能

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>这是一个井字棋游戏</h3>
+    <div class="wrap">
+      <div class="row" v-for="(row,index) in map" :key="index">
+        <Cell 
+          v-for="(cell,cindex) in row" 
+          :key="cindex" 
+          :text="cell"
+          @click="handleClick(row,index,cindex)"
+        />
+      </div>
+    </div>
+    <div class="result"> {{ result }} 胜利了 </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cell from './components/Cell.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      count: 0,
+      result: '',
+      map: [
+        ['','',''],
+        ['','',''],
+        ['','','']
+      ]
+    }
+  },
+  methods: {
+    handleClick(row,x,y){
+      let text = this.count % 2 === 1 ? 'o' : 'x'
+      this.count ++
+      this.$set(row, y, text)
+
+      // 判断输赢
+      const map = this.map
+      if(map[x][0] === map[x][1] && map[x][0] === map[x][2]){  // 所在行
+        this.result = text
+      }
+
+      if(map[0][y] === map[1][y] && map[0][y] === map[2][y]){  // 所在列
+        this.result = text
+      }
+
+      if(x + y === 2){    // 判断右上-左下 对角线
+        if(map[0][2] === map[1][1] && map[0][2] === map[2][0]){
+          this.result = text
+        }
+      }
+
+      if(x - y === 0){    // 判断左上-右下 对角线
+        if(map[0][0] === map[1][1] && map[0][0] === map[2][2]){
+          this.result = text
+        }
+      }
+    },
+  },
   components: {
-    HelloWorld
+    Cell
   }
 }
 </script>
@@ -25,4 +76,17 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+#app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.row {
+  display: flex;
+}
+.result {
+  font-size: 20px;
+  margin-top: 20px;
+};
 </style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cell" @click="clickCell">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["text"],
+  methods: {
+    clickCell(){
+      if(!this.text){
+        this.$emit('click')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cell {
+  border: 1px solid #000;
+  width: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 26px;
+}
+</style>
